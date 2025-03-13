0c3a0533445fc0fdeb47d01686e03ad9814b4b5f
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-13 10:39:46 +0300

    Add Store class with product listing functionality
    Added Store class, listProducts method, and sample product instances.

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -3,10 +3,18 @@ package org.example.model;
 public class Store {
     public static void main(String[] args) {
 
-        //listProducts();
+        ProductForSale[] products = {
+                new Chocolate(5.99, "Dark Chocolate", 70),
+                new Coke(1.99, "Refreshing Soda", false),
+                new Bread("Whole Grain Bread",2.49 , "Whole Wheat")
+        };
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        for (ProductForSale product : products) {
+            product.showDetails();
+            System.out.println("***********************");
+        }
     }
 }
